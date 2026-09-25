50d4676ebd9f5c0444a3fce94de99b6aa94de0cd
Olorunfemi <akanbi398@gmail.com>
2026-09-25 23:34:41 +0100

    Comment out duplicate imports and app initialization

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const express = require("express");
 require("dotenv").config();
 const router = express.Router();
 
-const app = express();
+// const app = express();
 
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+// const express = require("express");
+// const path = require("path");
+// const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
